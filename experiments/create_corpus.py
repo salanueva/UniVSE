dc73a265c55879424c34c22fd84cd8e042182e01
Ander Salaberria <andersalaberria@gmail.com>
2020-06-30 17:46:04 +0200

    Add argument parser to corpus creator script.

--- experiments/create_corpus.py
+++ experiments/create_corpus.py
@@ -1,47 +1,91 @@
+import argparse
+import numpy as np
 from pycocotools.coco import COCO
 from tqdm import tqdm
 import os
 import sys
 
 sys.path.append(os.getcwd())
-# from helper import load_data as ld
+from helper import load_data as ld
+from models.simplified_univse import corpus as simp_univse
 from models.univse import corpus as univse
 
 # IMPORTANT: As it is, this script creates vocabulary based on MS-Coco and vSTS datasets
 
+
+def parse_args():
+
+    parser = argparse.ArgumentParser(description='Pretrain a given VSE model with MS-COCO dataset')
+
+    parser.add_argument(
+        "--split",
+        type=str,
+        choices=["train", "restval", "all", "all+vsts"],
+        default="train",
+        help='COCO split used to create corpus.'
+    )
+    parser.add_argument(
+        '--simple',
+        default=False,
+        action='store_true',
+        help='Use it if you want to use a simplified version of UniVSE. False by default.'
+    )
+    parser.add_argument(
+        "--filename",
+        type=str,
+        default='corpus_univse.pickle',
+        help='Path for output files.'
+    )
+
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
 
-    # FIXME: Add argument parser so that directories are given as input parameters of the script
-    """
-    glove_dir = "/home/ander/Documentos/Datuak/glove/glove.840B.300d.txt"
-    train_images = "/home/ander/Documentos/Datuak/mscoco/train2014"
-    train_ann = "/home/ander/Documentos/Datuak/mscoco/annotations/captions_train2014.json"
-    """
+    args = parse_args()
+
+    # FIXME: Change argument parser so that directories are given as input parameters of the script
     glove_dir = "/gscratch/users/asalaberria009/embeddings/glove.840B.300d.txt"
     train_images = "/gscratch/users/asalaberria009/datasets/mscoco/train2014"
     train_ann = "/gscratch/users/asalaberria009/datasets/mscoco/annotations/captions_train2014.json"
-    # val_images = "/home/ander/Documentos/Datuak/mscoco/val2014"
-    # val_ann = "/home/ander/Documentos/Datuak/mscoco/annotations/captions_val2014.json"
-
-    cap_2014 = COCO(train_ann)
-    # cap_2017 = COCO(val_ann)
-
-    # cap_vsts_tr, cap_vsts_de, cap_vsts_te = ld.download_and_load_vsts_dataset(v2=True, images=False,
-    #                                                                           root_path="/home/ander/Documentos/Datuak")
+    val_images = "/gscratch/users/asalaberria009/datasets/mscoco/val2014"
+    val_ann = "/gscratch/users/asalaberria009/datasets/mscoco/annotations/captions_val2014.json"
 
     sentences = []
-    for idx in tqdm(cap_2014.anns.keys(), desc="MSCOCO Train"):
-        sentences.append(cap_2014.anns[idx]['caption'])
+    data_1 = COCO(train_ann)
+    data_2 = COCO(val_ann)
+    data_3 = {}
+    data_4 = {}
+    data_5 = {}
 
-    """
-    for idx in tqdm(cap_2017.anns.keys(), desc="MSCOCO Val"):
-        sentences.append(cap_2017.anns[idx]['caption'])
+    if args.split == "restval":
+        ids = {k: 0 for k in list(np.load('data/coco_restval_ids.npy'))}
+        for k in list(data_2.anns.keys()):
+            if k not in ids:
+                del data_2.anns[k]
+    else:
+        if args.split == "all+vsts":
+            data_3, data_4, data_5 = ld.download_and_load_vsts_dataset(
+                v2=True, images=False, root_path="/home/ander/Documentos/Datuak"
+            )
 
-    sentences += list(cap_vsts_tr["sent_1"]) + list(cap_vsts_tr["sent_2"]) + \
-        list(cap_vsts_de["sent_1"]) + list(cap_vsts_de["sent_2"]) + \
-        list(cap_vsts_te["sent_1"]) + list(cap_vsts_te["sent_2"])
-    """
+    sentences = []
+    for idx in tqdm(data_1.anns.keys(), desc="MSCOCO Train"):
+        sentences.append(data_1.anns[idx]['caption'])
 
-    voc_encoder = univse.VocabularyEncoder(sentences, glove_dir)
-    print(f"Num Objects: {len(voc_encoder.neg_obj)}")
-    voc_encoder.save_corpus('corpus_univse.pickle')
+    if args.split != "train":
+        for idx in tqdm(data_2.anns.keys(), desc="MSCOCO Val"):
+            sentences.append(data_2.anns[idx]['caption'])
+
+    if args.split == "all+vsts":
+        sentences += list(data_3["sent_1"]) + list(data_3["sent_2"]) + \
+            list(data_4["sent_1"]) + list(data_4["sent_2"]) + \
+            list(data_5["sent_1"]) + list(data_5["sent_2"])
+
+    if args.simple:
+        voc_encoder = simp_univse.VocabularyEncoder(sentences, glove_dir)
+    else:
+        voc_encoder = univse.VocabularyEncoder(sentences, glove_dir)
+        print(f"Num Objects: {len(voc_encoder.neg_obj)}")
+
+    voc_encoder.save_corpus(os.path.join('/gscratch/users/asalaberria009/datasets', args.filename))
